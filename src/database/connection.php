439fd5a20c3fd20f187f3022c99ada3f1449b21d
destinyarx <ditablangiljeremy@gmail.com>
2022-07-23 19:08:58 +0800

    database update

---
+++ src/database/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$host       = "localhost";
+$dbUser     = "root";
+$dbPassword = "";
+$dbName     = "hotel";
+
+
+try{ 
+    $connect = new PDO("mysql: host=$host; dbname=$dbName",$dbUser,$dbPassword);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}catch(Exception $e){
+    Echo "Connection failed" . $e->getMessage();  
+}
+
+?>
